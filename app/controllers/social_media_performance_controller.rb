99df8f86c403725da1315ffad7e1a65467168570
Ganesh Maduranathan <ganesh.masa92@gmail.com>
2016-11-15 17:22:54 +0530

    Social Media Performance

--- app/controllers/social_media_performance_controller.rb
+++ app/controllers/social_media_performance_controller.rb
@@ -1,2 +1,6 @@
 class SocialMediaPerformanceController < ApplicationController
+  def index
+    iris   = IrisEngine::IrisApi.new
+    @tagging_metrics = iris.case_study_search("tagging_metrics", {})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :attributes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/case_study_library" => "case_study_library#index", as: "case_study_library"
+  get "/social_media_performance" => "social_media_performance#index", as: "social_media_performance"
 end

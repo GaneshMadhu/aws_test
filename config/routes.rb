c7db04481e3d96b57564050ef272e7af5a7ac9b8
Ganesh Maduranathan <ganesh.maduranathan@universumglobal.com>
2016-11-16 15:20:30 +0530

    Show/Hide Description JS break fix

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   get "/case_study_library" => "case_study_library#index", as: "case_study_library"
   get "/social_media_performance" => "social_media_performance#index", as: "social_media_performance"
   get "/post_metrics" => "post_metrics#index", as: "post_metrics"
+
+  root "dashboard#index"
 end

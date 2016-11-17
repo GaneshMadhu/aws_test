e59dab1fb91118abf8629453d543517628b50e6e
supraja-universum <supraja.suresh@universumglobal.com>
2016-11-17 14:09:57 +0530

    Resolve merge conflicts

--- app/controllers/case_study_library_controller.rb
+++ app/controllers/case_study_library_controller.rb
@@ -1,23 +1,23 @@
 class CaseStudyLibraryController < ApplicationController
 
 	def index
-		iris   = IrisEngine::IrisApi.new
+		iris = IrisEngine::IrisApi.new
     @posts = iris.search("report_tagged_posts", {"sort"=> {"engagement_score_normalised"=>"desc"}})
 	end
-
+	
 	def filter
 		filter_data
 	end
 
-  def scroll
-    filter_data
-  end
+	def scroll
+		filter_data
+	end
 
-  private
+	private
 
-  def filter_data
-    filters = ParseQueryParams.new(params).parse
-    iris    = IrisEngine::IrisApi.new
-    @posts  = iris.search("report_tagged_posts/filter", filters)["data"]
-  end
+	def filter_data
+		filter_params = ParseQueryParams.new(params).parse
+		iris = IrisEngine::IrisApi.new
+    @posts = iris.search("report_tagged_posts/filter", filter_params)
+	end
 end

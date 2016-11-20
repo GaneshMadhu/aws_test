0b3ce5dccd2a369c9141dc4ebd1d346a6ffa44bb
Ganesh Maduranathan <ganesh.masa92@gmail.com>
2016-11-20 09:00:23 +0530

    filters for esmp and zoomin

--- app/controllers/social_media_performance_controller.rb
+++ app/controllers/social_media_performance_controller.rb
@@ -1,9 +1,12 @@
 class SocialMediaPerformanceController < ApplicationController
   def index
-    iris   = IrisEngine::IrisApi.new
+    iris             = IrisEngine::IrisApi.new
     @tagging_metrics = iris.search("tagging_metrics", {})
   end
 
   def filter
+    filters           = ParseQueryParams.new(params).parse
+    iris              = IrisEngine::IrisApi.new
+    @tagging_metrics  = iris.search("tagging_metrics/filter", filters)
   end
 end

2fe2dffcb07276a3ed9d6763a744e561d2cad32b
Ganesh Maduranathan <ganesh.maduranathan@universumglobal.com>
2016-11-21 15:39:32 +0530

    datepicker timeout

--- app/assets/javascripts/components/date_picker.js.jsx
+++ app/assets/javascripts/components/date_picker.js.jsx
@@ -6,36 +6,38 @@ var DatePicker = React.createClass({
 });
 
 function apply_date_picker(){
-  var timeInputFilter = $('#ugf-time'),
-      timeElFilter = $('#ugf-time-formatted');
+  setTimeout(function(){
+    var timeInputFilter = $('#ugf-time'),
+        timeElFilter = $('#ugf-time-formatted');
 
-  var startDate = new Date(),
-      endDate = new Date();
+    var startDate = new Date(),
+        endDate = new Date();
 
-  timeElFilter.find('.start').text('');
-  timeElFilter.find('.end').text('');
+    timeElFilter.find('.start').text('');
+    timeElFilter.find('.end').text('');
 
-  timeInputFilter.datepicker({
-      range: true,
-      date: new Date(),
-      language: 'en',
-      dateFormat: '',
-      startDate: startDate,
-      onSelect: function(formatted, date, inst) {
-          var dates = formatted.split(',');
-          timeElFilter.find('.start').text(dates[0]);
-          timeElFilter.find('.end').text(dates[1]);
-          timeInputFilter.trigger('change');
-      }
-  });
+    timeInputFilter.datepicker({
+        range: true,
+        date: new Date(),
+        language: 'en',
+        dateFormat: '',
+        startDate: startDate,
+        onSelect: function(formatted, date, inst) {
+            var dates = formatted.split(',');
+            timeElFilter.find('.start').text(dates[0]);
+            timeElFilter.find('.end').text(dates[1]);
+            timeInputFilter.trigger('change');
+        }
+    });
 
-  $(document).on('click', function(event) {
-      if (!$(event.target).is('#ugff-time-id, #ugff-time-id *, #datepickers-container, #datepickers-container *'))
-          if (timeInputFilter.length)
-              timeInputFilter.data('datepicker').hide();
-  });
-  timeElFilter.on('click', function(event) {
-      event.preventDefault();
-      timeInputFilter.data('datepicker').show();
-  });
+    $(document).on('click', function(event) {
+        if (!$(event.target).is('#ugff-time-id, #ugff-time-id *, #datepickers-container, #datepickers-container *'))
+            if (timeInputFilter.length)
+                timeInputFilter.data('datepicker').hide();
+    });
+    timeElFilter.on('click', function(event) {
+        event.preventDefault();
+        timeInputFilter.data('datepicker').show();
+    });
+  },500);
 }

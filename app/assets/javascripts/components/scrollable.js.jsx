aa9d51ff6dbc1945fed0bffa35f4c58910ef7234
Ganesh Maduranathan <ganesh.maduranathan@universumglobal.com>
2016-11-25 14:48:54 +0530

    scroll issue fix

--- app/assets/javascripts/components/scrollable.js.jsx
+++ app/assets/javascripts/components/scrollable.js.jsx
@@ -1,14 +1,20 @@
 var Scrollable = React.createClass({
   render: function(){
     var api_path = this.props.api_path;
-    $(window).scroll(function(){
-      if ($(window).scrollTop() == $(document).height() - $(window).height()){
-        var page = $('#post_current_page').val();
-        if(page >= $('#post_total_page').val()) return false;
-        rest_api_call(api_path, page);
-      }
+    window.requestAnimationFrame(function() {
+      trigger_scroll(api_path);
     });
-    remove_loaders();
     return null;
   }
 });
+
+function trigger_scroll(api_path){
+  $(window).scroll(function(){
+    if ($(window).scrollTop() == $("#page").height() - $(window).height()){
+      var page = $('#post_current_page').val();
+      if(page >= $('#post_total_page').val()) return false;
+      rest_api_call(api_path, page);
+    }
+  });
+  remove_loaders();
+}

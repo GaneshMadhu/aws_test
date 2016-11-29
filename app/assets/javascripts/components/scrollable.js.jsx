dbceb14fdebb39d901653f37bddb160f68d6d322
Ganesh Maduranathan <ganesh.masa92@gmail.com>
2016-11-29 08:48:23 +0530

    hope it fixes the scroll issue

--- app/assets/javascripts/components/scrollable.js.jsx
+++ app/assets/javascripts/components/scrollable.js.jsx
@@ -10,12 +10,16 @@ var Scrollable = React.createClass({
 });
 
 function trigger_scroll(api_path){
+  var timeout;
   $(window).scroll(function(){
-    if ($(window).scrollTop() == $("#page").height() - $(window).height()){
-      var page = $('#post_current_page').val();
-      if(page >= $('#post_total_page').val()) return false;
-      rest_api_call(api_path, page);
-    }
+    clearTimeout(timeout);
+    timeout = setTimeout(function() {
+      if ($(window).scrollTop() == $("#page").height() - $(window).height()){
+        var page = $('#post_current_page').val();
+        if(page >= $('#post_total_page').val()) return false;
+        rest_api_call(api_path, page);
+      }
+    }, 100);
   });
   remove_loaders();
 }

5a426abc4fb280ac1c49bb7c5f5445490ca19929
Ganesh Maduranathan <ganesh.masa92@gmail.com>
2016-12-05 10:30:41 +0530

    ESMP single company handling

--- app/controllers/social_media_performance_controller.rb
+++ app/controllers/social_media_performance_controller.rb
@@ -1,12 +1,33 @@
 class SocialMediaPerformanceController < ApplicationController
+
+  before_action :set_company_name, only: [:index]
+  before_action :set_company_filter
+
   def index
+    filters          = ParseQueryParams.new(params).parse
     iris             = IrisEngine::IrisApi.new
-    @tagging_metrics = iris.search("tagging_metrics", {})
+    @tagging_metrics = iris.search("tagging_metrics", filters)
   end
 
   def filter
     filters           = ParseQueryParams.new(params).parse
     iris              = IrisEngine::IrisApi.new
     @tagging_metrics  = iris.search("tagging_metrics/filter", filters)
+  end
+
+  private
+
+  def set_company_name
+    session['company_name'] = params['company_name']
+  end
+
+  def set_company_filter
+    if session['company_name']
+      if params['filter'].blank?
+        params['filter'] = {'company_name': [session['company_name']]}
+      else
+        params['filter']['company_name'] = [session['company_name']] if params['filter']['company_precode'].blank?
+      end
+    end
   end
 end

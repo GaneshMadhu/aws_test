406f8a482d4111c520df52682a82b45e3c2aed8c
Ganesh Maduranathan <ganesh.maduranathan@universumglobal.com>
2016-12-08 12:00:23 +0530

    flag-icon css added

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( utils/datepicker.js )
+Rails.application.config.assets.precompile += %w( flag-icon.css )

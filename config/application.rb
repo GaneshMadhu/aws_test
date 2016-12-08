149d35ddfd5d038fa3370a05cc09add70eeeea0a
Ganesh Maduranathan <ganesh.maduranathan@universumglobal.com>
2016-12-08 11:57:21 +0530

    flag-icon css added

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,5 @@ module IrisFe
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    Rails.application.config.assets.precompile += %w( flag-icon.css )
   end
 end

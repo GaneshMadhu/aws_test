d6caf2d0ca567464bcdced2b68181ce90d411279
Ganesh Maduranathan <ganesh.maduranathan@universumglobal.com>
2016-12-15 14:15:15 +0530

    CSL unit testing

--- spec/controllers/case_study_library_controller_spec.rb
+++ spec/controllers/case_study_library_controller_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe CaseStudyLibraryController, type: :controller do
+  describe 'index' do
+    context 'without filter_query' do
+      it 'gets the API response' do
+        get :index
+        expect(controller.instance_variable_get(:@posts).count).to be > 0
+        expect(controller.instance_variable_get(:@posts)['data'].count).to be > 5
+      end
+    end
 
+    context 'with filter_query applied' do
+      it 'gets the API response' do
+        get :index, {"filter_query"=>{"trait.code"=>"Ps"}}
+        expect(controller.instance_variable_get(:@posts).count).to be > 0
+        expect(controller.instance_variable_get(:@posts)['data'].count).to be > 0
+      end
+    end
+  end
+
+  describe 'filter' do
+    it 'filters the data' do
+      xhr :post, :filter, {"filter"=>{"trait.code"=>["Fg"]}}
+      expect(controller.instance_variable_get(:@posts).count).to be > 0
+      expect(controller.instance_variable_get(:@posts)['data'].count).to eq(0)
+    end
+  end
 end

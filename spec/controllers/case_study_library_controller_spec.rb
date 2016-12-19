41d43ca3fc3b968ad81fecd444b509d0f60327ca
Ganesh Maduranathan <ganesh.maduranathan@universumglobal.com>
2016-12-19 16:52:31 +0530

    ESMP Unit Testing

--- spec/controllers/case_study_library_controller_spec.rb
+++ spec/controllers/case_study_library_controller_spec.rb
@@ -5,16 +5,18 @@ RSpec.describe CaseStudyLibraryController, type: :controller do
     context 'without filter_query' do
       it 'gets the API response' do
         get :index
-        expect(controller.instance_variable_get(:@posts).count).to be > 0
-        expect(controller.instance_variable_get(:@posts)['data'].count).to be > 5
+        posts = controller.instance_variable_get(:@posts)
+        expect(posts.count).to be > 0
+        expect(posts['data'].count).to be > 5
       end
     end
 
     context 'with filter_query applied' do
       it 'gets the API response' do
         get :index, {"filter_query"=>{"trait.code"=>"Ps"}}
-        expect(controller.instance_variable_get(:@posts).count).to be > 0
-        expect(controller.instance_variable_get(:@posts)['data'].count).to be > 0
+        posts = controller.instance_variable_get(:@posts)
+        expect(posts.count).to be > 0
+        expect(posts['data'].count).to be > 0
       end
     end
   end
@@ -22,8 +24,9 @@ RSpec.describe CaseStudyLibraryController, type: :controller do
   describe 'filter' do
     it 'filters the data' do
       xhr :post, :filter, {"filter"=>{"trait.code"=>["Fg"]}}
-      expect(controller.instance_variable_get(:@posts).count).to be > 0
-      expect(controller.instance_variable_get(:@posts)['data'].count).to eq(0)
+      posts = controller.instance_variable_get(:@posts)
+      expect(posts.count).to be > 0
+      expect(posts['data'].count).to eq(0)
     end
   end
 end
--- spec/controllers/social_media_performance_controller_spec.rb
+++ spec/controllers/social_media_performance_controller_spec.rb
@@ -2,4 +2,32 @@ require 'rails_helper'
 
 RSpec.describe SocialMediaPerformanceController, type: :controller do
 
+  before(:each) do
+    session['company_name']       = 'Google'
+    session['company_group_name'] = 'India'
+  end
+
+  describe 'index' do
+    context 'without filter_query' do
+      it 'gets the API response' do
+        get :index
+        tagging_metrics = controller.instance_variable_get(:@tagging_metrics)
+        expect(tagging_metrics.count).to be > 0
+        expect(tagging_metrics['data']['tags_count']).to be_kind_of(Integer)
+        expect(tagging_metrics['data']['tags']).to be_kind_of(Hash)
+        expect(tagging_metrics['data']['tags']).to be_truthy
+      end
+    end
+  end
+
+  describe 'filter' do
+    it 'filters the data' do
+      xhr :post, :filter, {"filter"=>{"company_precode"=>["0123"]}}
+      tagging_metrics = controller.instance_variable_get(:@tagging_metrics)
+      expect(tagging_metrics.count).to be > 0
+      expect(tagging_metrics['data']['tags_count']).to be_kind_of(Integer)
+      expect(tagging_metrics['data']['tags']).to be_nil
+      expect(tagging_metrics['data']['tags']).not_to be_truthy
+    end
+  end
 end

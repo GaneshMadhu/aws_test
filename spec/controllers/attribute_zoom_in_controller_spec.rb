b5e19d0f59503f7a1ba89b83643ab74593fb858d
Ganesh Maduranathan <ganesh.maduranathan@universumglobal.com>
2017-01-11 11:49:06 +0530

    code refactoring and indentation

--- spec/controllers/attribute_zoom_in_controller_spec.rb
+++ spec/controllers/attribute_zoom_in_controller_spec.rb
@@ -4,8 +4,10 @@ RSpec.describe AttributeZoomInController, type: :controller do
 
   before {controller.class.skip_before_filter :authenticate_user!}
 
-  let(:default_trait){"Ps"}
-  let(:empty_trait){"Xs"}
+  let(:default_trait)    { "Ps" }
+  let(:empty_trait)      { "Xs" }
+  let(:post_metrics)     { controller.instance_variable_get(:@post_metrics) }
+  let(:post_metric_data) { post_metrics['data'] }
 
   describe '#index' do
     context 'without filter_query' do
@@ -17,18 +19,16 @@ RSpec.describe AttributeZoomInController, type: :controller do
     context 'with filter_query applied' do
       it 'gets the API response' do
         get :index, {"filter_query":{"trait.code": default_trait, "post_time": {'max': Date.today.strftime("%m/%d/%y"), 'min': (Date.today - 5.years).strftime("%m/%d/%y")}}}
-        @post_metrics = controller.instance_variable_get(:@post_metrics)
-        expect(@post_metrics['data']['avg_time'][0]['view_posts']).to include(default_trait)
+        expect(post_metric_data['avg_time'][0]['view_posts']).to include(default_trait)
       end
     end
 
     after(:each) do
-      post_metrics = @post_metrics || controller.instance_variable_get(:@post_metrics)
       expect(post_metrics.count).to be > 0
-      expect(post_metrics['data']['avg_engagement']).to be_kind_of(Float)
-      expect(post_metrics['data']).to have_key("avg_time")
-      expect(post_metrics['data']['avg_time']).to be_kind_of(Array)
-      expect(post_metrics['data']['avg_time']).to be_truthy
+      expect(post_metric_data['avg_engagement']).to be_kind_of(Float)
+      expect(post_metric_data).to have_key("avg_time")
+      expect(post_metric_data['avg_time']).to be_kind_of(Array)
+      expect(post_metric_data['avg_time']).to be_truthy
       expect(response).to be_success
       expect(response).to have_http_status(200)
       expect(response).to render_template('index')
@@ -39,23 +39,21 @@ RSpec.describe AttributeZoomInController, type: :controller do
     context 'filters the trait data' do
       it 'for empty trait data' do
         xhr :post, :filter, {"filter":{"trait.code":[empty_trait]}}
-        post_metrics = controller.instance_variable_get(:@post_metrics)
         expect(post_metrics.count).to be > 0
-        expect(post_metrics['data']['avg_engagement']).to be_kind_of(Float)
-        expect(post_metrics['data']).to have_key("avg_time")
-        expect(post_metrics['data']['avg_time']).to be_empty
-        expect(post_metrics['data']['avg_time']).to be_truthy
+        expect(post_metric_data['avg_engagement']).to be_kind_of(Float)
+        expect(post_metric_data).to have_key("avg_time")
+        expect(post_metric_data['avg_time']).to be_empty
+        expect(post_metric_data['avg_time']).to be_truthy
       end
 
       it 'for non-empty trait data' do
         xhr :post, :filter, {"filter":{"trait.code":[default_trait]}}
-        post_metrics = controller.instance_variable_get(:@post_metrics)
         expect(post_metrics.count).to be > 0
-        expect(post_metrics['data']['avg_engagement']).to be_kind_of(Float)
-        expect(post_metrics['data']).to have_key("avg_time")
-        expect(post_metrics['data']['avg_time'][0]['view_posts']).to include(default_trait)
-        expect(post_metrics['data']['avg_time']).not_to be_empty
-        expect(post_metrics['data']['avg_time']).to be_truthy
+        expect(post_metric_data['avg_engagement']).to be_kind_of(Float)
+        expect(post_metric_data).to have_key("avg_time")
+        expect(post_metric_data['avg_time'][0]['view_posts']).to include(default_trait)
+        expect(post_metric_data['avg_time']).not_to be_empty
+        expect(post_metric_data['avg_time']).to be_truthy
       end
     end
 
--- spec/controllers/case_study_library_controller_spec.rb
+++ spec/controllers/case_study_library_controller_spec.rb
@@ -4,26 +4,26 @@ RSpec.describe CaseStudyLibraryController, type: :controller do
 
   before {controller.class.skip_before_filter :authenticate_user!}
 
-  let(:default_trait){"Ps"}
-  let(:empty_trait){"Xs"}
+  let(:default_trait){ "Ps" }
+  let(:empty_trait)  { "Xs" }
+  let(:posts)        { controller.instance_variable_get(:@posts) }
+  let(:post_data)    { posts['data'] }
 
   describe '#index' do
     context 'without filter_query' do
       it 'gets the API response' do
         get :index
-        posts = controller.instance_variable_get(:@posts)
         expect(posts.count).to be > 0
-        expect(posts['data'].count).to be > 5
+        expect(post_data.count).to be > 5
       end
     end
 
     context 'with filter_query applied' do
       it 'gets the API response' do
         get :index, {"filter_query":{"trait.code": default_trait}}
-        posts = controller.instance_variable_get(:@posts)
         expect(posts.count).to be > 0
-        expect(posts['data'][0]['trait']['code']).to eq(default_trait)
-        expect(posts['data'].count).to be > 0
+        expect(post_data[0]['trait']['code']).to eq(default_trait)
+        expect(post_data.count).to be > 0
       end
     end
 
@@ -38,17 +38,15 @@ RSpec.describe CaseStudyLibraryController, type: :controller do
     context 'filters the trait data' do
       it 'for empty data' do
         xhr :post, :filter, {"filter":{"trait.code":[empty_trait]}}
-        posts = controller.instance_variable_get(:@posts)
         expect(posts.count).to be > 0
-        expect(posts['data'].count).to eq(0)
+        expect(post_data.count).to eq(0)
       end
 
       it 'for non-empty trait data' do
         xhr :post, :filter, {"filter":{"trait.code":[default_trait]}}
-        posts = controller.instance_variable_get(:@posts)
         expect(posts.count).to be > 0
-        expect(posts['data'][0]['trait']['code']).to eq(default_trait)
-        expect(posts['data'].count).to be > 0
+        expect(post_data[0]['trait']['code']).to eq(default_trait)
+        expect(post_data.count).to be > 0
       end
     end
 
